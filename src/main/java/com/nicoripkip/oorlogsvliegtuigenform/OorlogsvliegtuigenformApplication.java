fc319c4dca504842e2ecd78f2fafa86389d42c66
nicoripkip <nicovanommen.nvo@gmail.com>
2021-12-06 17:54:14 +0100

    Spring boot files toegevoegd

---
+++ src/main/java/com/nicoripkip/oorlogsvliegtuigenform/OorlogsvliegtuigenformApplication.java
@@ -0,0 +1,13 @@
+package com.nicoripkip.oorlogsvliegtuigenform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OorlogsvliegtuigenformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OorlogsvliegtuigenformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicoripkip/oorlogsvliegtuigenform/OorlogsvliegtuigenformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoripkip.oorlogsvliegtuigenform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OorlogsvliegtuigenformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
